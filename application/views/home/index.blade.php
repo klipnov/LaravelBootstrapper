85b26487732f69aecfafc71288a62a224df71ae4
Qays <qays@qaysBuntu.(none)>
2013-01-17 22:44:36 +0800

    sync

---
+++ application/views/home/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>DigitalArk</title>
+		{{ Asset::styles() }}
+	</head>
+
+	<body>
+		<div class="container arkMainDiv">
+			<h1>DigitalArk - <small>Sailing the sea of creativity</small></h1>
+
+		</div>
+	</body>
+</html>
